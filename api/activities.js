10634cae6173f9886c1dabacc1b525a421f21fbc
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2020-09-04 05:52:12 +0500

    use JWT
    Updated authentication to use JWT instead of stateful sessions.
    
    Also created env variables instead of hardcoding secrets

--- api/activities.js
+++ api/activities.js
@@ -1,42 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const authenticationHelpers = require("./helpers/authenticationHelpers");
-
 const Activity = require("../models/activity");
 
 router.get("/", (req, res) => {
-  if (!authenticationHelpers.checkIfUnauthorizedRequest(req, res)) {
-    Activity.find()
-      .then((users) => res.json(users))
-      .catch((err) => console.log(err));
-  }
+  Activity.find()
+    .then((users) => res.json(users))
+    .catch((err) => console.log(err));
 });
 
 router.post("/", (req, res) => {
-  if (!authenticationHelpers.checkIfUnauthorizedRequest(req, res)) {
-    const { activityName, activityType } = req.body;
-    const newActivity = new Activity({
-      userID: req.session.userID,
-      activityName: activityName,
-      activityType: activityType,
-      pings: 0,
-      createdAt: new Date(),
-    });
-    newActivity
-      .save()
-      .then(() =>
-        res.json({
-          message: "Created activity successfully",
-        })
-      )
-      .catch((err) =>
-        res.status(400).json({
-          error: err,
-          message: "Error creating activity",
-        })
-      );
-  }
+  const { activityName, activityType } = req.body;
+  const newActivity = new Activity({
+    userID: req.user.userID,
+    activityName: activityName,
+    activityType: activityType,
+    pings: 0,
+    createdAt: new Date(),
+  });
+  newActivity
+    .save()
+    .then(() =>
+      res.json({
+        message: "Created activity successfully",
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        error: err,
+        message: "Error creating activity",
+      })
+    );
 });
 
 module.exports = router;
