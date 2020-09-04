bb851586762d4af90442e925ba30fea62bc50c1b
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2020-09-04 08:00:54 +0500

    Use ENV variables for MongoDB

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const connection =
-  "mongodb+srv://test_user:EFLpLWWNYAyy7zlI@test.tcfm8.mongodb.net/Vibelist?retryWrites=true&w=majority";
+const connection = `mongodb+srv://${process.env.MONGODB_ACCESS_USER_NAME}:${process.env.MONGODB_ACCESS_PASSWORD}@test.tcfm8.mongodb.net/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(connection, {
